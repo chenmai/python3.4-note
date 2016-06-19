e0dc5b577f63968e5dcde9ec2e354081c309bd95
chenmai <chenmai98@qq.com>
2016-06-19 23:56:18 +0800

    Signed-off-by: chenmai <chenmai98@qq.com>

--- spider-note/日常脚本/360网盘每日自动抽奖.py
+++ spider-note/日常脚本/360网盘每日自动抽奖.py
@@ -6,13 +6,14 @@ Created on 2016年6月17日
 from selenium import webdriver
 import os
 from time import sleep
+from selenium.webdriver.common.action_chains import ActionChains
 
 #mac下的设置
-chromedriver = "/Users/duomai/Documents/chromedriver/chromedriver"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chromedriver)
+# chromedriver = "/Users/duomai/Documents/chromedriver/chromedriver"
+# os.environ["webdriver.chrome.driver"] = chromedriver
+# driver = webdriver.Chrome(chromedriver)
 
-# driver = webdriver.Chrome()
+driver = webdriver.Chrome()
 
 driver.get('http://c99.yunpan.360.cn/')
 
@@ -23,9 +24,19 @@ driver.find_element_by_name("account").send_keys("18814887520") #输入用户名
 driver.find_element_by_name("password").send_keys("chenmai") #输入密码
 
 
-driver.find_element_by_xpath("//*[@id='login']/div/div[2]/form/p[5]/input").click() #点击登录
+# driver.find_element_by_xpath("//*[@id='login']/div/div[2]/form/p[5]/input").click() #点击登录
 
-sleep(5)
+sleep(20)
+
+m=driver.find_element_by_xpath("//*[@id='topPanel']/div/div")
+driver.find_element_by_xpath("//*[@id='topPanel']/div/div").click()
+ActionChains(driver).move_to_element(m)
+print(m.find_element_by_xpath("//*[@id='lottery-everyday']/i").is_displayed())
+# js="var q=m.find_element_by_xpath(\"//*[@id='lottery-everyday']/i\").menu .icon, .menu .text ={    cursor: pointer;}"
+# driver.execute_script(js)
+print(m.find_element_by_xpath("//*[@id='lottery-everyday']/i").is_displayed())
+m.find_element_by_xpath("//*[@id='lottery-everyday']/i").click()
+
 
 # 验证码导致延后
 #driver.find_element_by_xpath("//*[@id='lottery-everyday']/i").click()
